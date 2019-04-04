95be570cfdb7cbce84ec2cfe56e90989bcd7f423
Jakub Łokieć <jakublokiec@wp.pl>
2019-04-04 17:22:22 +0200

    Initialize spinners with currency codes

--- app/src/main/java/pl/jlokiec/currencyconverter/MainActivity.java
+++ app/src/main/java/pl/jlokiec/currencyconverter/MainActivity.java
@@ -1,7 +1,9 @@
 package pl.jlokiec.currencyconverter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeSpinners();
+    }
+
+    private void initializeSpinners() {
+        Spinner spinnerCurrencyFrom = findViewById(R.id.spinner_currency_from_code);
+        Spinner spinnerCurrencyTo = findViewById(R.id.spinner_currency_to_code);
+
+        ArrayAdapter<CharSequence> currencyCodesAdapter = ArrayAdapter.createFromResource(this, R.array.currency_codes, android.R.layout.simple_spinner_item);
+        currencyCodesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerCurrencyFrom.setAdapter(currencyCodesAdapter);
+        spinnerCurrencyTo.setAdapter(currencyCodesAdapter);
     }
 }
